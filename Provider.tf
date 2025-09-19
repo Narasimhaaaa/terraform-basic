958bd964067838b6225e260a41301c4ef8cf13e0
Narasimha <naras@example.com>
2025-09-19 12:05:09 +0530

    add

--- Provider.tf
+++ Provider.tf
@@ -94,5 +94,7 @@ resource "aws_instance" "instance_name" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   subnet_id = aws_subnet.publicn_subnet.id
 
-  
+  tags = {
+    name ="Mywebserve"
+  }
 }
